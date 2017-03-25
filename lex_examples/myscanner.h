8a8930bb8bd7c6d3730c005317fc88507c79546c
kuannie1 <anne.ku4@gmail.com>
2017-03-25 19:33:03 -0400

    added examples using lex

---
+++ lex_examples/myscanner.h
@@ -0,0 +1,8 @@
+// Defining a symbol for each token in the config.in file
+#define TYPE 1
+#define NAME 2
+#define TABLE_PREFIX 3
+#define PORT 4
+#define COLON 5
+#define IDENTIFIER 6
+#define INTEGER 7
