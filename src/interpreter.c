7d5b53e15af1cc6b854f59e1beb30d4065e61dc0
Serena Chen <serenazychen@gmail.com>
2017-04-20 15:02:42 -0400

    wrote eval

--- src/interpreter.c
+++ src/interpreter.c
@@ -19,8 +19,22 @@ void free_code(char* code){
     free(code);
 }
 
-float eval(Exp* ast){
+float eval(Ast_Node* ast){
+    // return value
+    if(ast->func == FLT){
+        return ast->value->flt;
+    }
 
+    // binary functions
+    float left_val = eval(ast->left);
+    float right_val = eval(ast->right);
+
+    switch(ast->func){
+        case MULT:  return left_val * right_val;
+        case ADD:   return left_val + right_val;
+    }
+    perror("why are we here");
+    exit(-1);
 }
 
 int main(int argc, char** argv){
@@ -32,9 +46,20 @@ int main(int argc, char** argv){
     char* code = get_code(filename);
 
     //TODO: pass to parser/lexer, get AST
-    Exp* ast;
+    Ast_Node* ast = malloc(sizeof(Ast_Node));
+    ast->func = ADD;
+    ast->left = malloc(sizeof(Ast_Node));
+    ast->right = malloc(sizeof(Ast_Node));
+
+    ast->left->func = FLT;
+    ast->left->value = malloc(sizeof(AstVal));
+    ast->left->value->flt = 5.5;
+    ast->right->func = FLT;
+    ast->right->value = malloc(sizeof(AstVal));
+    ast->right->value->flt = 6;
 
     float expression = eval(ast);
+    printf("%f\n", expression);
 
     return 0;
 }
