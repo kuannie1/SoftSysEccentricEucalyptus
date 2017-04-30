a5db5b2f09a8d1a975d885f96dc2f766bc601b13
margaretmcrawf <margaretmcrawf@gmail.com>
2017-04-30 11:51:16 -0400

    removed unnecessary parts of ast

--- ast_c/ast.h
+++ ast_c/ast.h
@@ -1,8 +1,7 @@
 typedef struct Exp {
 
 	enum { integer_exp, string_exp, variable_exp,
-		binary_exp, unary_exp, call_exp,
-		projection_exp, record_exp ,
+		binary_exp, unary_exp,
 		function_exp } tag;
 
 	union {
@@ -16,16 +15,6 @@ typedef struct Exp {
 		//a unary operation (like a negative sign)
 		struct { char* operator;
 			struct Exp* operand; } unaryExp;
-		//a call operation (like a function???)
-		struct { char* name;
-			struct Exp_list* arguments; } callExp;
-		//projection operation (still trying to figure out what this means)
-		struct {struct Exp* record;
-			char* attribute; } projectionExp;
-		//also not sure what this is
-		struct rec { char* attribute;
-			struct Exp* value;
-			struct rec* next; } recordExp;
 		//function
 		struct { char* name;
 			struct Exp* func;
@@ -35,17 +24,10 @@ typedef struct Exp {
 
 } ast;
 
-typedef struct Exp_list { 
-  ast*             elem;
-  struct Exp_list* next;
-} ast_list;
 
 ast *make_binaryExp ( char* operator, ast* left, ast* right );
 ast *make_unaryExp( char* operator, ast* operand );
 ast *make_integerExp( int integerExp );
 ast* make_stringExp( char* stringExp );
 ast* make_variableExp( char* variableExp );
-ast* make_callExp( char* name, ast_list* arguments );
-ast* make_projectionExp( ast* record, char* attribute );
-ast* make_recordExp( char* attribute, ast* value, struct rec* next );
 ast* make_functionExp( char* name, ast* func, int num_arguments, char* arguments[] );
